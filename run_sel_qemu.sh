09693a0313bec84e6350e1c38cfaab77bb07dbb0
Edward J. Schwartz <eschwartz@cert.org>
2023-04-05 10:38:50 -0400

    Improve run script

--- run_sel_qemu.sh
+++ run_sel_qemu.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-$( dirname "${BASH_SOURCE[0]}" )/build/arm-softmmu/qemu-system-m68k -cpu cfv4e -machine an5206 \
+$( dirname "${BASH_SOURCE[0]}" )/build/m68k-softmmu/qemu-system-m68k -cpu cfv4e -machine an5206 \
   -kernel /dev/null \
   -device loader,file=$(dirname "${BASH_SOURCE[0]}")/../jetset_private_data/sel/r2003cf1.bin,addr=0,force-raw=true  \
   -device loader,file=$(dirname "${BASH_SOURCE[0]}")/../jetset_private_data/sel/r512351s.bin.skipped,addr=0x100000,force-raw=true \
   -device loader,addr=0x420,cpu-num=0 \
-  -serial stdio \
-  -d nochain -singlestep -S -qmp tcp:localhost:$1,server,nowait
+  -d nochain -singlestep -S -qmp tcp:localhost:$1,server,nowait #2>&1 | tee /tmp/log
